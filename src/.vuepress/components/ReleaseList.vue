ea35ee5335cb2496e6b353630d2f8857f411f01b
Christian Hammacher <bmasterc@gmail.com>
2022-06-14 10:15:10 +0200

    Fixes for ReleaseList component

--- src/.vuepress/components/ReleaseList.vue
+++ src/.vuepress/components/ReleaseList.vue
@@ -1,33 +1,40 @@
 <template>
-  <div>
-    <h2>Releases</h2>
-    <li v-for="item in items" :key="item.id">
-      <a v-bind:href="item.zipball_url">{{ item.tag_name }}</a>
-    </li>
-  </div>
+	<div>
+		<h2>Releases</h2>
+		<ul>
+			<li v-for="item in items" :key="item.tagName">
+				<a :href="item.zipballUrl">
+					{{ item.tagName }}
+				</a>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-    if (typeof window !== "undefined"){
-      window.global = window;
-    }
+import fetch from 'cross-fetch';
 
 export default {
-  data() {
-    return {
-      items: [],
-    };
-  },
-  props: ['gituser', 'gitrepo'],
-  created(){
-    fetch(`https://api.github.com/repos/${this.gituser}/${this.gitrepo}/releases`)
-      .then(res => res.json())
-      .then(data => {
-        const items = data.map((x) => {
-          return {tag_name : x.tag_name, zipball_url: x.zipball_url}
-          })
-        this.$data.items = items
-    })
-  }
+	data() {
+		return {
+			items: [],
+		};
+	},
+	props: {
+		gituser: String,
+		gitrepo: String
+	},
+	mounted() {
+		fetch(`https://api.github.com/repos/${this.gituser}/${this.gitrepo}/releases`)
+			.then(res => res.json())
+			.then(data => {
+				this.$data.items = data.map(item => {
+					return {
+						tagName: item.tag_name,
+						zipballUrl: item.zipball_url
+					}
+				})
+			});
+	}
 };
 </script>
