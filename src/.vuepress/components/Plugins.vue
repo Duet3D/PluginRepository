41a515442066dada62f4202cd9bcee91e8579b41
dax <yasas.wijetilake@gmail.com>
2022-06-14 14:30:53 +0100

    Updating all plugins

--- src/.vuepress/components/Plugins.vue
+++ src/.vuepress/components/Plugins.vue
@@ -8,35 +8,20 @@
             <div class="div_1">
                 <div class="div_2">
                     <a target="_self" :href="`/PluginRepository/plugins/${item}.html`">
-                        <img src="https://static.npmjs.com/26de4581a89de8a6501ce9a3dbb06b85.svg" title="public package" class="img_1">
                         <h3 class="h3_class">{{item}}</h3>
                     </a>
                     <span id="pkg-list-exact-match" class="highlight_1">OEM</span>
                 </div>
-                <p class="description_1">Simple, synchronous deflate/inflate for buffers</p>
-                <div class="card_footer">
-                    <span title="Release and Date" aria-hidden="true" class="card_footer_1">🔖 v3.4 📅 2022-05-15</span>
-                </div>
-                <ul class="keyword_1">
-                    <li><a href="/search?q=keywords:zlib" class="keyword_list_1">zlib</a></li>
-                    <li><a href="/search?q=keywords:deflate" class="keyword_list_1">deflate</a></li>
+                <p class="description_1">Lorem ipsum lorem ipsum</p>
+                <ul class="card_footer">
+                    <li><span title="Published Date" class="card_footer_1">📅 2022-01-05</span></li>
+                    <li><span title="Release" class="card_footer_1">🔖 v3.4 </span></li>
+                    <li><span title="Last Release Date" class="card_footer_1">📅 2022-05-16</span></li>
                 </ul>
-            </div>
-            <div class="stats_1">
-                <div class="stats_2">
-                    <div class="stats_3" title="Maintenance 0%">
-                        <div class="stats_4 color_red" style="transform: scaleX(0.5);"></div>
-                        <div class="stats_4b">M</div>
-                    </div>
-                    <div class="stats_3" title="Quality 67%">
-                        <div class="stats_4 color_blue" style="transform: scaleX(0.67285);"></div>
-                        <div class="stats_4b">Q</div>
-                    </div>
-                    <div class="stats_3" title="Popularity 32%">
-                        <div class="stats_4 color_green" style="transform: scaleX(0.319384);"></div>
-                        <div class="stats_4b">P</div>
-                    </div>
-                </div>
+                <ul class="keyword_1">
+                    <li><a href="/search?q=tag:keyword_1" class="keyword_list_1">keyword_1</a></li>
+                    <li><a href="/search?q=tag:keyword_2" class="keyword_list_1">keyword_2</a></li>
+                </ul>
             </div>
         </section>
 	</div>
