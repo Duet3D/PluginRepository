855e96175e75f0d3e7301776e1d6fcae85ab067d
dax <yasas.wijetilake@gmail.com>
2022-07-12 15:26:04 +0100

    precheck script

--- extra/precheck.sh
+++ extra/precheck.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env sh
 
+touch checklog.txt
+
 # 1. Check for illegal characters (allow only alphanumeric GH user+repo+branch names)
 
 # 2. Check if README.md or PLUGIN.md is present on the specified repo
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null https://api.github.com/repos/Duet3D/DuetSoftwareFramework/contents/README.md)
-    # if 200, then file exists
+    status_code=$(curl --write-out %{http_code} --silent --output /dev/null https://api.github.com/repos/Duet3D/DuetSoftwareFramework/contents/README.md)
+
+    res=$(if [[ "$status_code" = 200 ]]; then echo "OK"; else echo "NOT OK"; fi)
+    echo README.md: $res >> checklog.txt
 
 # 3. Make sure there is at least one release with at least one ZIP file
-url=$(curl https://api.github.com/repos/Duet3D/InputShapingPlugin/releases | jq .[0]."assets"[0]."browser_download_url" | sed 's/"//g')
+    url=$(curl https://api.github.com/repos/Duet3D/InputShapingPlugin/releases | jq .[0]."assets"[0]."browser_download_url" | sed 's/"//g')
     #if url exists, then at least one asset is there
     ext=$(echo $url | rev | cut -d'.' -f 1 |rev)
     #check if ext is zip to confirm it's a zip
+    res=$(if [[ "$ext" = zip ]]; then echo "OK"; else echo "NOT OK"; fi)
+    echo Release: $res >> checklog.txt
 
 # 4. Download latest ZIP bundle
     wget $url -O asset.zip
@@ -18,12 +24,24 @@ url=$(curl https://api.github.com/repos/Duet3D/InputShapingPlugin/releases | jq 
     cd assetunzipped
 
 # 5. Make sure plugin.json exists
-    (ls plugin.json >> /dev/null 2>&1  && echo yes) || echo no #save this output somewhere and check if plugin.json exists
+    res=$((ls plugin.json >> /dev/null 2>&1  && echo "OK") || echo "NOT OK")
+    echo plugin.json check 1: $res >> checklog.txt
 
 # 6. Ensure properties id, name, author are present. Validate them as specified in https://github.com/Duet3D/DuetWebControl/blob/master/src/plugins/manifest.js
-    plugin_id=$(cat plugin.json | jq ."id" | sed 's/"//g' | awk '{print length}') #Check if less than 32 characters
+    plugin_id=$(cat plugin.json | jq ."id" | sed 's/"//g') #Check if less than 32 characters
+    plugin_id_n=$(echo $plugin_id | awk '{print length}')#Check if less than 32 characters
+    res=$(if [[ "$plugin_id" != null ]]&&[[ "$plugin_id_n" < 32 ]]; then echo "OK"; else echo "NOT OK"; fi)
+    echo plugin.json id: $res >> checklog.txt
+
     plugin_name=$(cat plugin.json | jq ."name" | sed 's/"//g' | awk '{print length}') #Check if less than 64 characters
+    plugin_name_n=$(echo $plugin_name | awk '{print length}') #Check if less than 64 characters
+    res=$(if [[ "$plugin_name" != null ]]&&[[ "$plugin_name_n" < 64 ]]; then echo "OK"; else echo "NOT OK"; fi)
+    echo plugin.json name: $res >> checklog.txt
+
     plugin_author=$(cat plugin.json | jq ."author" | sed 's/"//g' | awk '{print length}') #Check if empty
+    plugin_author_n=$(echo $plugin_name | awk '{print length}') #Check if empty
+    res=$(if [[ "$plugin_author" != null ]]&&[[ "$plugin_author_n" > 0 ]]; then echo "OK"; else echo "NOT OK"; fi)
+    echo plugin.json author: $res >> checklog.txt
 
 # 7. Check if at least one "version" dependency is present [dwcVersion, sbcDSfVersion, rrfVersion] and that each value starts with a number
     plugin_dwcVersion=$(cat plugin.json | jq ."dwcVersion")
