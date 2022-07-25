d04d6306626149038cf374fa7cb5514883a494fd
dax <yasas.wijetilake@gmail.com>
2022-07-25 19:18:29 +0100

    Updating sidebar thingy

--- src/.vuepress/theme/components/Page.vue
+++ src/.vuepress/theme/components/Page.vue
@@ -2,6 +2,10 @@
   <main class="page">
     <slot name="top" />
 
+    <div class="margindiv"/>
+
+    <div class="sidebar2">abc</div>
+    <div class="sidebar3">xyz</div>
     <Content class="theme-default-content" />
     <PageEdit />
 
@@ -37,5 +41,38 @@ $wrapper
 .page
   padding-bottom 2rem
   display block
+
+.margindiv
+  padding-top 25px
+
+
+.sidebar2
+  font-size 16px
+  background-color #00ff00
+  width $sidebarWidth
+  position fixed
+  z-index 10
+  margin 0
+  top $navbarHeight
+  left 0
+  bottom 0
+  box-sizing border-box
+  border-right 1px solid $borderColor
+  overflow-y auto
+
+
+.sidebar3
+  font-size 16px
+  background-color #00fff0
+  width $sidebarWidth
+  position fixed
+  z-index 10
+  margin 0
+  top $navbarHeight
+  right 0
+  bottom 0
+  box-sizing border-box
+  border-right 1px solid $borderColor
+  overflow-y auto
 
 </style>
