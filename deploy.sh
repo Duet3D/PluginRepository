eafd9f663135044f71ee37de6c526c9535bf61b1
dax <yasas.wijetilake@gmail.com>
2022-08-09 21:01:10 +0100

    New Deployment

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,7 @@ cd src/.vuepress/dist
 # mv assets ./src/assets
 # cd src
 
-git init
-git add -A
-git commit -m "New Deployment"
+git init && git add -A && git commit -m "New Deployment"
 
 git push -f git@ssh.github.com:Duet3D/PluginRepository.git master:gh-pages
 
