b56409729aec66d5ac24e94400101284e247216e
dax <yasas.wijetilake@gmail.com>
2022-08-16 00:24:02 +0100

    Updating footer

---
+++ src/.vuepress/components/AppReleaseVersion.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<h6 style="text-align: center">{{"Duet3D Limited 2022 | Plugin Repository Version: " + this.$data.version}}</h6>
+	</div>
+</template>
+
+<script>
+import fetch from 'cross-fetch';
+
+export default {
+	data() {
+		return {
+			items: [],
+            version: undefined
+		};
+	},
+	props: {
+		gituser: String,
+		gitrepo: String
+	},
+	mounted() {
+		fetch(`https://raw.githubusercontent.com/Duet3D/PluginRepository/master/package.json`)
+			.then(res => res.json())
+			.then(data => {
+                const {version} = data;
+                this.$data.version = version;
+			});
+	}
+};
+</script>
--- src/index.md
+++ src/index.md
@@ -2,16 +2,7 @@
 # layout: CustomHomeLayout
 home: true
 heroImage: https://avatars.githubusercontent.com/u/59487011
-# tagline: This is a demo repository
-# actionText: Discover Plugins →
-# actionLink: /plugins/
-# features:
-# - title: Our Products
-#   details: Feature 1 Description
-# - title: Forum
-#   details: Feature 2 Description
-# - title: GitHub
-#   details: Feature 3 Description
-footer: Duet3D 2022
+# footer: 
 ---
 <Plugins/>
+<AppReleaseVersion/>
