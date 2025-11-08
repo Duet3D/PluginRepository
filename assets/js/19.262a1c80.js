b6e5e90c6bc931bc96a0481f1276f35f705effd5
MintyTrebor <MintyTrebor@users.noreply.github.com>
2025-11-08 11:52:16 +0000

    deploy: 8a2387932619c518c84c86445f6d7de5be6992eb

--- assets/js/19.262a1c80.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{296:function(t,s,n){"use strict";n.r(s);var e=n(13),l=Object(e.a)({},(function(){var t=this._self._c;return t("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[t("Plugins"),this._v(" "),t("AppReleaseVersion")],1)}),[],!1,null,null,null);s.default=l.exports}}]);
